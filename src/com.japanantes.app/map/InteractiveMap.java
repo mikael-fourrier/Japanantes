5e74305af2296ac4399e0ea081e38ef9135d6717
Yamakaky <yamakaky@gmail.com>
2015-05-09 16:40:17 +0200

    Ajout du squelette de la View map interactive.

---
+++ src/com.japanantes.app/map/InteractiveMap.java
@@ -0,0 +1,17 @@
+package com.japanantes.app;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class InteractiveMap extends View {
+    public InteractiveMap(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+
+    }
+}
